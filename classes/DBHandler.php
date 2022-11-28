9222ac32d4cf5f8c0bbf52aee328bab1a971ed68
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2022-11-28 14:53:51 -0700

    created class for handling connection to database

---
+++ classes/DBHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+// THIS CLASS DEALS WITH CONNECTING TO THE DATABASE
+class DBHandler
+{
+
+    public function connect()
+    {
+        try {
+            $username = "root";
+            $password = "root";
+            // We are connecting to the 'gameplatform' database on localhost with the specified username and password
+            $dbh = new PDO('mysql:host=localhost;dbname=gameplatform', $username, $password);
+            return $dbh;
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+}
