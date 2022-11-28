7b91ef055d6a94324be9f5fe34078a69e801a604
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2022-11-28 13:42:27 -0700

    updated form fields

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ include "header.php";
     <div class="index-login-login">
         <h4>LOGIN</h4>
         <form action="includes/login.inc.php" method="post">
-            <input type="text" name="uid" placeholder="Username" />
+            <input type="text" name="email" placeholder="Email" />
             <input type="password" name="pwd" placeholder="Password" />
             <br>
             <button type="submit" name="submit">LOGIN</button>
--- signup.php
+++ signup.php
@@ -6,10 +6,9 @@ include "header.php";
     <div class="wrapper">
         <h4>SIGN UP</h4>
         <form action="includes/signup.inc.php" method="post">
-            <input type="text" name="uid" placeholder="Username" />
+            <input type="text" name="email" placeholder="Email" />
             <input type="password" name="pwd" placeholder="Password" />
             <input type="password" name="pwdRepeat" placeholder="Repeat Password" />
-            <input type="text" name="email" placeholder="Email" />
             <br>
             <button type="submit" name="submit">SIGN UP</button>
         </form>
