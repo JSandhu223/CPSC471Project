be2eb9635a2e80ddb3bd60a1e6a8313a9282442e
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2022-11-28 20:43:30 -0700

    created functions for validating email and password

--- classes/SignupController.php
+++ classes/SignupController.php
@@ -20,6 +20,18 @@ class SignupController
 
     public function initiateSignup()
     {
+        // Check if any of the input fields are empty
+        if ($this->emptyField()) {
+            // Send the user back to the sign up page with an error message in url
+            header("location: ../signup.php?error=emptyinputfield");
+            exit();
+        }
+
+        if (!$this->isValidUsername()) {
+            // Send the user back to the sign up page with an error message in url
+            header("location: ../signup.php?error=invalidusername");
+            exit();
+        }
     }
 
     private function emptyField()
@@ -40,6 +52,45 @@ class SignupController
 
         // The username may consist of only alphabet characters, digits 0-9, and underscores
         if (!preg_match("/^[a-zA-Z0-9_]*$/", $this->username)) {
+            $valid = false;
+        }
+
+        return $valid;
+    }
+
+    private function isValidEmail()
+    {
+        $valid = true;
+
+        if (!preg_match("/^[a-zA-Z0-9]+@[a-zA-Z]+.+c+o+m$/", $this->email)) {
+            $valid = false;
+        }
+
+        return $valid;
+    }
+
+    private function isValidPassword()
+    {
+        $valid = true;
+        $pattern = "/^[a-zA-Z0-9!@#$%^&*()_?]*$/";
+        $minLength = 10;
+
+        // Check if tbe password contains any illegal characters
+        if (!preg_match($pattern, $this->password)) {
+            $valid = false;
+        }
+
+        // Check if the password is long enough
+        if (strlen($this->password) < $minLength) {
+            $valid = false;
+        }
+
+        // Check if the password is not the same as the username
+        if ($this->password == $this->username) {
+            $valid = false;
+        }
+
+        if ($this->password == $this->email) {
             $valid = false;
         }
 
