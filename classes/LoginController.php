1f601f7bd0c78f63f6a3d2a73ca28111737c7eff
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2022-11-29 16:41:27 -0700

    Created controller for logging in

---
+++ classes/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+class LoginController extends LoginHandler
+{
+
+    private $username;
+    private $password;
+
+    // Constructor
+    // This is called when a user fills in the sign up page and hits the submit button
+    public function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function initiateSignup()
+    {
+        // Check if any of the input fields are empty
+        if ($this->emptyField()) {
+            // Send the user back to the sign up page with an error message in url
+            header("location: ../signup.php?error=emptyinputfield");
+            exit();
+        }
+
+        // If none of these errors handlers are triggered, then create the user
+        $this->loginUser($this->username, $this->password);
+    }
+
+    private function emptyField()
+    {
+        $isEmpty = false;
+        $emptyInput = "";
+
+        if ($this->username == $emptyInput || $this->email == $emptyInput || $this->password == $emptyInput || $this->passwordRepeat == $emptyInput) {
+            $isEmpty = true;
+        }
+
+        return $isEmpty;
+    }
+}
