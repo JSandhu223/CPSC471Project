b208d2b300670510cae6482f78dfc1a75f9b9150
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2022-11-28 21:14:29 -0700

    created class for adding user to database

---
+++ classes/SignupHandler.php
@@ -0,0 +1,27 @@
+<?php
+// THIS CLASS DEALS WITH QUERYING THE SIGN UP INFORMATION TO OUR DATABASE
+
+class SignupHander extends DBHandler
+{
+
+    public function createUser($username, $email, $password)
+    {
+        // Create a prepared statememt TO PREVENT SQL INJECTIONS
+        $stmt = $this->connect()->prepare("INSERT INTO USER (username, email, password) VALUES (?, ?, ?);");
+
+        // Execute the query, where these 3 arguments replace the question marks in our prepared statement
+        $query = $stmt->execute(array($username, $email, $password));
+
+        // If the query doesn't execute return an error
+        if (!$query) {
+            // Nullify the statement just to be safe
+            $stmt = null;
+            // Send the user to the signup page with an error
+            header("location: ../signup.php?failedtoexecutestatement");
+            exit();
+        }
+        
+        // If we reach here, it means the user was successfully inserted into the USER table
+        $stmt = null;
+    }
+}
