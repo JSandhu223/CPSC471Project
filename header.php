89f588a5d2c0db953b39d484d7d305a5a57d489d
jasraj.sandhu <jasraj.sandhu@cs.ucalgary.ca>
2022-11-29 18:29:29 -0700

    Added links for navigation bar, reformatted file

--- header.php
+++ header.php
@@ -2,31 +2,33 @@
 <html lang=en>
 
 <head>
-	<meta charset="UTF-8" />
-	<link rel="stylesheet" href="styles.css" />
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="styles.css" />
     <title>Project</title>
 </head>
 
 <body>
-	<header>
-		<nav>
-			<div>
-				<ul class="main-navbar">
-					<li><a href="index.php">Home</a></li>
-					<li><a href="#">Games</a></li>
-					<li><a href="signup.php">Sign Up</a></li>
-					<li><a href="login.php">Login</a></li>
-				</ul>
-				<ul class="user-navbar">
-					<?php
-						if (isset($_SESSION["username"])) {
-					?>
-							<li><a href="#"><?php echo $_SESSION["username"]; ?></a></li>
-							<li><a href="includes/logout.inc.php">Logout</a></li>
-					<?php
-						}
-					?>
-				</ul>
-			</div>
-		</nav>
-	</header>
+    <header>
+        <nav>
+            <div>
+                <ul class="main-navbar">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="signup.php">Sign Up</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+                <ul class="user-navbar">
+                    <?php
+                    if (isset($_SESSION["username"])) {
+                    ?>
+                        <li><a href="#"><?php echo $_SESSION["username"]; ?></a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Groups</a></li>
+                        <li><a href="#">Rate a Game</a></li>
+                        <li><a href="includes/logout.inc.php">Logout</a></li>
+                    <?php
+                    }
+                    ?>
+                </ul>
+            </div>
+        </nav>
+    </header>
